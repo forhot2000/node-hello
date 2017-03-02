7f804f93c41d6812285ab439e78f28a2a848bd7b
West Wang <west@fossil.com>
2017-03-02 12:40:04 +0800

    render env

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send([
+    '<h1>Hello World!<h1>', 
+    '<p><i>DOCKER_IMAGE: ', os.environ['DOCKER_IMAGE'], '</i></p>',
+    '<p><i>ENVIRONMENT: ', os.environ['ENVIRONMENT'], '</i></p>'
+  ].join('\n'));
 });
 
 app.get('/ping', function (req, res) {
